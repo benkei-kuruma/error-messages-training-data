f24eac7fca97b005379859ae0a4a6d5a1a645dd7
Benkei Kuruma <johndane66@gmail.com>
2023-01-27 09:37:16 -0800

    Update json_to_csv.py

--- json_to_csv.py
+++ json_to_csv.py
@@ -7,8 +7,7 @@ with open('errors.json') as json_file:
 csv_file = open('errors.csv', 'w')
 csv_writer = csv.writer(csv_file)
 
-my_data = []
 for key in data.keys():
-    for value in data[key].values():
-        csv_writer.writerow(value)
+  for another_key in data[key].keys():
+    csv_writer.writerow(data[key][another_key][0].upper())
 csv_file.close()
